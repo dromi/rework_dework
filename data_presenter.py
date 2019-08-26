81d161622444bb222221ba577246f75d80ad557a
dromi <danni.dromi@gmail.com>
2019-08-26 23:19:05 +0200

    Add visual component using pygame

--- data_presenter.py
+++ data_presenter.py
@@ -1,8 +1,12 @@
+import sys
 from configparser import ConfigParser
 from random import choices, choice
 import time
 
+import pygame
+
 from database.db_dude import DBDude
+from scroll_text import ScrollText
 
 WEIGHTS = {
     'financial_private': 1.0,
@@ -11,32 +15,102 @@ WEIGHTS = {
     'political': 1.0
 }
 
+white = (255, 255, 255)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+red = (255, 0, 0)
+
+COLORS = {
+    'financial_private': blue,
+    'financial_public': blue,
+    'environmental': red,
+    'political': green
+}
+
+
+pygame.init()
 
 class DataPresenter():
     def __init__(self, config_path):
         self.config = ConfigParser()
         self.config.read(config_path)
 
-        self.round_sleep_time = self.config.getint('presenter', 'sleep_time')
-
         self.dude = DBDude(self.config['general']['db_file'])
+
+        self.sleep_time = self.config.getfloat('presenter', 'sleep_time')
+        self.screen_width = self.config.getint('presenter', 'screen_width')
+        self.screen_height = self.config.getint('presenter', 'screen_height')
+        self.font = self.config['presenter']['font']
+        self.font_size = self.config.getint('presenter', 'font_size')
+        self.margin = self.config.getint('presenter', 'margin')
+        self.delta_y = self.config.getint('presenter', 'delta_y')
+        self.margin = self.config.getint('presenter', 'margin')
+
+        self.line_length = self._determine_line_chars()
+        self.scrolling_texts = []
 
     def fetch_next(self):
         category = choices(list(WEIGHTS.keys()), weights=WEIGHTS.values())[0]
         if category == 'financial_public':
-            return choice(self.dude.select_all_financial_public())
+            chosen = choice(self.dude.select_all_financial_public())
         elif category == 'financial_private':
-            return choice(self.dude.select_all_financial_private())
+            chosen = choice(self.dude.select_all_financial_private())
         elif category == 'environmental':
-            return choice(self.dude.select_all_environmental())
+            chosen = choice(self.dude.select_all_environmental())
         elif category == 'political':
-            return choice(self.dude.select_all_political())
+            chosen = choice(self.dude.select_all_political())
         else:
             print("Error: Choice not recognized")
+            return None, None
+        return chosen.__str__(), COLORS[category]
 
     def run(self):
-        #TODO find better condition
+        screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+        font = pygame.font.SysFont(self.font, self.font_size)
+
         while True:
-            next_item = self.fetch_next()
-            print(next_item)
-            time.sleep(self.round_sleep_time)
+            # check for quit events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    # TODO: Find a way for this to work also on a multi thread setup
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        self.delta_y = 0 if self.delta_y == 1 else 1
+
+            screen.fill(0)
+
+            # MAIN LOOP DESCRIPTION:
+            #
+            # Check that there is a scrolltext above the screen
+            # if not add one
+            # translate all items down
+            # check if any have gone off screen
+            # If so move to top of stack (find the one with min y and move above him)
+
+            if not any(text.top_y < 0 for text in self.scrolling_texts):
+                text_item, color = self.fetch_next()
+                self.scrolling_texts.append(ScrollText(text_item, self.line_length, font, color, self.margin))
+
+            for text_scroll in self.scrolling_texts:
+                text_scroll.translate(self.delta_y)
+                text_scroll.render(screen)
+                if text_scroll.top_y > self.screen_height:
+                    offset = min([text.top_y for text in self.scrolling_texts])
+                    text_scroll.reset(abs(offset), self.fetch_next())
+            pygame.display.update()
+            time.sleep(self.sleep_time)
+
+    def _determine_line_chars(self):
+        font = pygame.font.SysFont(self.font, self.font_size)
+        char_length = 1
+        while font.size("A"*char_length)[0] < (self.screen_width - 2 * self.margin):
+            char_length += 1
+        print(char_length - 1)
+        return char_length - 1
+
+
+if __name__ == '__main__':
+    presenter = DataPresenter('config.ini')
+    presenter.run()
---
+++ scroll_text.py
@@ -0,0 +1,44 @@
+class ScrollText:
+    def __init__(self, text: str, line_length: int, font, color, margin):
+        self.full_text = text
+        self.line_length = line_length
+        self.font = font
+        self.color = color
+        self.margin = margin
+        self.text_lines = self._split_string()
+        self.text_objects = [self.font.render(t, True, self.color) for t in self.text_lines]
+        self.height = (len(self.text_objects) * self.text_objects[0].get_size()[1]) + self.margin
+        self.top_y = 0 - self.height
+
+    def _split_string(self):
+        words = self.full_text.split()
+        lines = []
+        current_line = []
+        while len(words) > 0:
+            section_len = sum([len(s) for s in current_line]) + len(current_line) - 1
+            if section_len + len(words[0]) + 1 <= self.line_length:
+                current_line.append(words[0])
+                words.pop(0)
+            else:
+                lines.append(' '.join(current_line))
+                current_line = []
+        if len(current_line) > 0:
+            lines.append(' '.join(current_line))
+        return lines
+
+    def translate(self, delta_y):
+        self.top_y += delta_y
+
+    def render(self, screen):
+        line_height = self.text_objects[0].get_size()[1]
+        for i, text in enumerate(self.text_objects):
+            screen.blit(text, (self.margin, i * line_height + self.top_y))
+
+    def reset(self, offset, new_item):
+        text, color = new_item
+        self.full_text = text
+        self.color = color
+        self.text_lines = self._split_string()
+        self.text_objects = [self.font.render(t, True, self.color) for t in self.text_lines]
+        self.height = (len(self.text_objects) * self.text_objects[0].get_size()[1]) + self.margin
+        self.top_y = 0 - self.height - offset
